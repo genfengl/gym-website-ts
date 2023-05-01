95a46e3b4875ee9fdc5a8578c53f92e4cbf333cc
genfengl <genfeng.liu@gmail.com>
2023-05-01 23:17:12 +1000

    sponsors in home also finished

--- src/scenes/home/Sponsors.tsx
+++ src/scenes/home/Sponsors.tsx
@@ -3,30 +3,32 @@ import SponsorForbes from "../../assets/SponsorForbes.png";
 import SponsorFortune from "../../assets/SponsorFortune.png";
 import SponsorRunner from "../../assets/SponsorRunner.png";
 import SponsorZenYoga from "../../assets/SponsorZenYogo.png";
+import SponsorVitality from "../../assets/SponsorVitality.png";
 
 const Sponsors = () => {
   return (
-    <div className="hidden h-24 w-full items-center justify-center bg-primary-300 md:flex">
-      <div className="flex w-5/6">
-        <div className="flex h-8 w-full justify-between gap-16">
-          <img
-            src={SponsorRedBull}
-            alt="redbull-sponsor"
-            className="object-contain"
-          />
-          <img
-            src={SponsorForbes}
-            alt="forbes-sponsor"
-            className="object-contain"
-          />
-          <img
-            src={SponsorFortune}
-            alt="fortune-sponsor"
-            className="object-contain"
-          />
-          <img src={SponsorRunner} alt="runner-sponsor" />
-          <img src={SponsorZenYoga} alt="zenyoga-sponsor" />
-        </div>
+    // Container
+    <div className="hidden h-36 w-full items-center justify-center bg-primary-300 md:flex">
+      {/* Inner container */}
+      <div className="flex h-8 w-5/6 justify-between gap-16">
+        <img
+          src={SponsorRedBull}
+          alt="redbull-sponsor"
+          className="object-contain"
+        />
+        <img
+          src={SponsorForbes}
+          alt="forbes-sponsor"
+          className="object-contain"
+        />
+        <img
+          src={SponsorFortune}
+          alt="fortune-sponsor"
+          className="object-contain"
+        />
+        <img src={SponsorRunner} alt="runner-sponsor" />
+        <img src={SponsorZenYoga} alt="zenyoga-sponsor" />
+        <img src={SponsorVitality} alt="vitality-sponsor" />
       </div>
     </div>
   );
